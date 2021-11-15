381a6ebdacdf57d4c30c1fd26b88d59ca3449fd0
Alexa Macias <Alexa Macias>
2021-11-15 18:56:08 -0500

    whoops

--- config/database.js
+++ config/database.js
@@ -1,6 +1,6 @@
 // config/database.js
 module.exports = {
 
-    'url' : 'mongodb+srv://demo:demo@cluster0.q2ojb.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', 
-    'dbName': 'demo'
+    'url' : 'mongodb+srv://user-one:BlueMoon@cluster0.5xllp.mongodb.net/roamHistory?retryWrites=true&w=majority', 
+    'dbName': 'roamHistory'
 };
--- public/main.js
+++ public/main.js
@@ -1,7 +1,40 @@
-var thumbUp = document.getElementsByClassName("fa-thumbs-up");
-var trash = document.getElementsByClassName("fa-trash");
 
-Array.from(thumbUp).forEach(function(element) {
+
+const map = L.map('map').setView([21.346982, 105.326259], 5);
+setTimeout(() =>{map.flyTo([10, 10], 3)},2000) //setTimeout method an anonymous function and the time as the two parameters
+//L = leaflet (the API)
+//L.map([id of the div designated to be where the map is]) <--initialize map
+//.setView <- another method that is being called upon the map that is setting up the initial coordinates and the zoom level
+var Stadia_OSMBright = L.tileLayer('https://tiles.stadiamaps.com/tiles/osm_bright/{z}/{x}/{y}{r}.png', {
+	maxZoom: 20,
+	attribution: '&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
+});
+//^^creating a variable of the base map
+
+
+map.addLayer(Stadia_OSMBright)
+let circle = L.circle([21.346982, 105.326259], { //the circle is a function requiring two arguments. in this case, the coordinates in the [] make up the first one, aka the location of the shape/center of the circle
+  color: 'red', //this block makes up the entirety of the second argument being passed through the circle function
+  fillColor: '#f03',
+  fillOpacity: 0.5,
+  radius: 200000
+}).addTo(map); 
+
+circle.bindTooltip("Hồng Bàng dynasty, c. 2879 (?)–258 BC", {permanent : true}).openTooltip(); //another function. this time, the argument will be the message that will appear inside the popup
+//.bindTooltip = another method, applied to the circle variable.
+
+circle.bindPopup(`
+TITLE. Or whatever else.
+<img class='' src='https://upload.wikimedia.org/wikipedia/commons/5/50/World_500_BCE_showing_Van_Lang.png'>
+<span>Save for reference</span>
+`, 
+{}); //another function. this time, the argument will be the message that will appear inside the popup
+//.bindTooltip = another method, applied to the circle variable.
+
+
+
+
+ Array.from(thumbUp).forEach(function(element) {
       element.addEventListener('click', function(){
         const name = this.parentNode.parentNode.childNodes[1].innerText
         const msg = this.parentNode.parentNode.childNodes[3].innerText
@@ -42,4 +75,4 @@ Array.from(trash).forEach(function(element) {
           window.location.reload()
         })
       });
-});
+}); 
